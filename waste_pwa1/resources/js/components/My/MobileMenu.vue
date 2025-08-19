afebf69f97bd0bd4bbdf238df404333edd0a682b
uuganbayar davaasuren <u0690b@gmail.com>
2025-08-20 06:00:19 +0800

    medku medku

--- waste_pwa1/resources/js/components/My/MobileMenu.vue
+++ waste_pwa1/resources/js/components/My/MobileMenu.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { NavItem } from '@/types';
+import { usePage } from '@inertiajs/vue3';
 
 
 defineProps<{ menus: NavItem[] }>();
-
+const page =usePage()
 </script>
 
 <template>
@@ -14,12 +15,14 @@ defineProps<{ menus: NavItem[] }>();
             <div class="grid h-full max-w-lg grid-cols-5 mx-auto">
                 <template v-for="menu in menus" :key="menu.href">
                     <UTooltip :text="menu.title">
-                        <ULink target="_self" :href="menu.href" active-class="bg-gray-200"
-                            class="inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50 data-active:bg-black dark:hover:bg-gray-800 group">
+                        <ILink :href="menu.href" active-class="bg-gray-200"
+                            class="inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50 data-active:bg-black dark:hover:bg-gray-800 group"
+                            :class="{ 'bg-gray-200': menu.href === page.url }">
+
 
                             <component :is="menu.icon" />
                             <span class="sr-only"> {{ menu.title }}</span>
-                        </ULink>
+                        </ILink>
                     </UTooltip>
 
                 </template>
